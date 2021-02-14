b4167e8ab6a2acb9ea1f47e446387dadd329e560
devi.sada <devi.sada@gmail.com>
2021-02-13 19:17:07 -0500

    Add ninja class assignment

---
+++ ninja/ninja.js
@@ -0,0 +1,18 @@
+class ninja{
+    constructor(name,health,speed,strength){
+        this.name = name;
+        this.health =health;
+        this.speed = speed;
+        this.strength = strength;
+    }
+    sayName(){
+        console.log(`Name ${this.name}`);
+    }
+    showStats(){
+        console.log(`Name ${this.name}, Health ${this.health}, Speed ${this.speed}, Strength ${this.strength}`);
+    }
+    drinkSake(){
+        this.health += 10;
+        return this;
+    }
+}
