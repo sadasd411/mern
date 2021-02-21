25f5f6d4e64b740299ee6175d68ac27b7d8d1719
devi.sada <devi.sada@gmail.com>
2021-02-21 17:15:46 -0500

    My first react app

---
+++ react-apps/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="MyFirstAPP">
+    <h1>Hello Dojo</h1>
+    <h3>Things I need to do</h3>
+    <li>
+      <ul> * Learn react</ul>
+      <ul>* Climb Mt.Everest</ul>
+      <ul>* Run a marathon</ul>
+      <ul>* Feed a Dog</ul>
+    </li>
+    </div>
+  );
+}
+
+export default App;
