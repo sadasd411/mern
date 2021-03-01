210b51aa71401cf3a32559d340cd4037650bec40
devi.sada <devi.sada@gmail.com>
2021-02-28 19:54:19 -0500

    More forms assignment

--- hook-form/src/Components/hook.js
+++ hook-form/src/Components/hook.js
@@ -6,20 +6,59 @@ const HookForm =(props)=>{
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");  
     const [confirmPassword, setConfirmPassword] = useState("");  
+    const [firstNameError, setFirstNameError] = useState("");
+    const [lastNameError, setLastNameError ] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const handleFisrtName = (e) => {
+        setFirstname(e.target.value);
+        if(e.target.value.length < 1) {
+            setFirstNameError("First name is required!");
+        } else if(e.target.value.length < 2) {
+            setFirstNameError("First name  must be 3 characters or longer!");
+        }
+    }
+    const handleLastName = (e) => {
+        setLastname(e.target.value);
+        if(e.target.value.length < 1) {
+            setLastNameError("Last name is required!");
+        } else if(e.target.value.length < 2) {
+            setLastNameError("Last name  must be 3 characters or longer!");
+        }
+    }
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+        if(e.target.value.length < 1) {
+            setEmailError("Email is required!");
+        } else if(e.target.value.length < 5) {
+            setEmailError("Email  must be 5 characters or longer!");
+        }
+    }
     return(
         <div>
             <form>
                 <div>
                     <label>First Name :</label>
-                    <input type="text" onChange={(e)=>setFirstname(e.target.value)}/>
+                    <input type="text" onChange={handleFisrtName}/>
+                    {
+                        firstNameError ?
+                        <p style={{color:'red'}}>{ firstNameError }</p> : null
+                    }
                 </div>
                 <div>
                     <label>Last Name :</label>
-                    <input type="text" onChange={(e)=>setLastname(e.target.value)}/>
+                    <input type="text" onChange={handleLastName}/>
+                    {
+                        lastNameError ?
+                        <p style={{color: 'red'}}>{lastNameError}</p> : null
+                    }
                 </div>
                 <div>
                     <label>Email Address :</label>
-                    <input type="text" onChange={(e)=>setEmail(e.target.value)}/>
+                    <input type="text" onChange={handleEmail}/>
+                    {
+                        emailError ?
+                        <p style={{color: 'red'}} > {emailError}</p> : " "
+                    }
                 </div>
                 <div>
                     <label>Password :</label>
